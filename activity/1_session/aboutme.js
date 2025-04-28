3edc3a7108382f7778fbead36932c00b8f11a1c2
DanTell <tello.di@gmail.com>
2025-04-27 22:54:17 -0500

    Update aboutme.js

--- activity/1_session/aboutme.js
+++ activity/1_session/aboutme.js
@@ -2,4 +2,8 @@ const fullName = "Daniel Tello Paute";
 const profession = "Project Manager";
 const occupation = "Full Stack Developer";
 
-console.log(`Hola, me llamo ${fullName} y me dedico a ${profession}. Estoy cursando este Máster porque me gustaría trabajar en ${occupation}.`);
+const mensaje = `Hola, me llamo ${fullName} y me dedico a ${profession}. Estoy cursando este Máster porque me gustaría trabajar como ${occupation}.`;
+
+console.log(mensaje);
+
+export default mensaje;
